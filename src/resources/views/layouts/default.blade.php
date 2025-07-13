e920d74d3a3d817a6d2309f5c4fbb3d12e06eea7
muto ryoko <obng3prn3@gmail.com>
2025-07-14 03:25:12 +0900

    login,registerのvalidation作成

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -13,13 +13,12 @@
     <header class="header">
         <div class="header__inner">
             <a href=""><img src="{{ asset('storage/materials/logo.svg') }}" alt="ロゴ"></a>
-            {{-- @if (Auth::check()) --}}
             <div class="login-user__function">
                 <form class="search-form" action="" method="GET">
                     <input class="search-form__input" type="text" name="keyword" placeholder="なにをお探しですか？">
                 </form>
                 <div class="login-out__btn">
-                    <a href="">ログアウト</a>
+                    <a href="">ログイン・ログアウト</a>
                 </div>
                 <div class="mypage__btn">
                     <a href="">マイページ</a>
@@ -28,7 +27,6 @@
                     <a href="">出品</a>
                 </div>
             </div>
-            {{-- @endif --}}
         </div>
     </header>
 
