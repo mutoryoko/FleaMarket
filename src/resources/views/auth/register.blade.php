16449c0238e013cae04bfac200c989bda7673fdb
muto ryoko <obng3prn3@gmail.com>
2025-07-14 17:41:39 +0900

    register.css編集

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -8,17 +8,25 @@
 
 @section('content')
     <div class="content">
-        <h2>会員登録</h2>
+        <h2 class="title">会員登録</h2>
         <form class="register-form" action="" method="POST">
         @csrf
-            <label class="form-label"><div>ユーザー名</div></label>
-            <input type="text" name="name" value="{{ old('name') }}" />
-            <label class="form-label"><div>メールアドレス</div></label>
-            <input type="text" name="email" value="{{ old('email') }}" />
-            <label class="form-label"><div>パスワード</div></label>
-            <input type="password" name="password" />
-            <label class="form-label"><div>確認用パスワード</div></label>
-            <input type="password" name="password_confirmation" />
+            <div class="form-item">
+                <label class="form-label"><div>ユーザー名</div></label>
+                <input class="form-item__input" type="text" name="name" value="{{ old('name') }}" />
+            </div>
+            <div class="form-item">
+                <label class="form-label"><div>メールアドレス</div></label>
+                <input class="form-item__input" type="text" name="email" value="{{ old('email') }}" />
+            </div>
+            <div class="form-item">
+                <label class="form-label"><div>パスワード</div></label>
+                <input class="form-item__input" type="password" name="password" />
+            </div>
+            <div class="form-item">
+                <label class="form-label"><div>確認用パスワード</div></label>
+                <input class="form-item__input" type="password" name="password_confirmation" />
+            </div>
 
             <div class="register-form__btn">
                 <button class="register-form__btn--submit" type="submit">登録する</button>
