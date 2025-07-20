5fc1f0d908b0da79c06b04f0fbd3a7ce94058e44
muto ryoko <obng3prn3@gmail.com>
2025-07-20 21:08:49 +0900

    mypageのblade編集

--- src/app/Http/Controllers/MypageController.php
+++ src/app/Http/Controllers/MypageController.php
@@ -13,7 +13,10 @@ class MypageController extends Controller
 {
     public function index()
     {
-        return view('mypage.profile');
+        $user = Auth::user();
+        $profile = $user->profile;
+
+        return view('mypage.profile', compact('user', 'profile'));
     }
 
     public function edit()
--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -3,7 +3,37 @@
 @section('title', 'プロフィール')
 
 @section('css')
+    <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
 @endsection
 
 @section('content')
+<div class="content">
+    <div class="profile">
+        <div class="profile-image__wrapper">
+            <img class="profile-image" src="{{ asset('storage/'.$profile->user_image) }}" alt="プロフィール画像">
+        </div>
+        <h2 class="user-name">{{ $user->name }}</h2>
+        <div class="edit-btn">
+            <a class="edit-btn--link" href="{{ route('edit') }}">プロフィールを編集</a>
+        </div>
+    </div>
+    <div class="item-list__tabs">
+        <a href="">出品した商品</a>
+        <a href="">購入した商品</a>
+    </div>
+    <div class="item-cards__wrapper">
+        @if(!empty($items))
+            @foreach ($items as $item)
+            <div class="item-card">
+                <div class="item-image__wrapper">
+                    <a href="{{ route('detail', ['item_id' => $item->id]) }}"><img class="item-image" src="{{ asset('storage/'.$item->image) }}" alt="商品画像"></a>
+                </div>
+                <h2 class="item-name">{{ $item->name }}</h2>
+            </div>
+            @endforeach
+        @elseif(empty($items))
+        <p>商品がありません</p>
+        @endif
+    </div>
+</div>
 @endsection
