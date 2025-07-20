e20706e80d333f86bce1b4e1b41761804bad965c
muto ryoko <obng3prn3@gmail.com>
2025-07-20 19:29:39 +0900

    UsersTableSeederのpasswordをハッシュ化

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,35 +15,35 @@ class UsersTableSeeder extends Seeder
             [
                 'name' => '山田太郎',
                 'email' => 'taro@example.com',
-                'password' => 'password1',
+                'password' => Hash::make('password1'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => '田中次郎',
                 'email' => 'jiro@test.com',
-                'password' => 'password2',
+                'password' => Hash::make('password2'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => '佐々木三郎',
                 'email' => 'saburo@laravel.co.jp',
-                'password' => 'password3',
+                'password' => Hash::make('password3'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => '鈴木四郎',
                 'email' => 'shiro@sample.com',
-                'password' => 'password4',
+                'password' => Hash::make('password4'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'name' => '稲垣五郎',
                 'email' => 'goro@laravel.com',
-                'password' => 'password5',
+                'password' => Hash::make('password5'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
