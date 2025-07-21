0fedd6f463156cd99eb817fcb38f3dc64b144727
muto ryoko <obng3prn3@gmail.com>
2025-07-21 17:31:28 +0900

    CategoriesItemsSeeder作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             ProfilesTableSeeder::class,
             ItemsTableSeeder::class,
             CategoriesTableSeeder::class,
+            CategoriesItemsTableSeeder::class,
         ]);
     }
 }
