2ac38695a7de3360217209f886878a819e767bde
muto ryoko <obng3prn3@gmail.com>
2025-07-24 18:08:09 +0900

    購入画面のview作成

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -4,13 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Item;
+use App\Models\Profile;
+use Illuminate\Support\Facades\Auth;
 
 class PurchaseController extends Controller
 {
-    public function index()
+    public function index(string $id)
     {
-        $item = Item::all();
+        $item = Item::findOrFail($id);
+        $user = Auth::user();
+        $profile = $user->profile;
 
-        return view('purchase', compact('item'));
+        return view('purchase', compact('item', 'user', 'profile'));
+    }
+
+    public function edit()
+    {
+        $user = Auth::user();
+        $profile = $user->profile;
+
+        return view('address', compact('user', 'profile'));
     }
 }
--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -7,4 +7,46 @@
 @endsection
 
 @section('content')
+    <div class="content">
+        <div class="confirm">
+            <div class="item-info">
+                <div>
+                    <img src="" alt="">
+                </div>
+                <h2>{{ $item->item_name }}</h2>
+                <p><span class="yen">¥</span>{{ $item->price }}</p>
+            </div>
+            <div class="payment-method">
+                <h3>支払い方法</h3>
+                <select name="payment_method">
+                    <option>選択してください</option>
+                    <option value="1">コンビニ払い</option>
+                    <option value="2">カード払い</option>
+                </select>
+            </div>
+            <div class="shipping-info">
+                <div class="shipping__head">
+                    <h3>配送先</h3>
+                    <a href="">変更する</a>
+                </div>
+                <p>{{ $profile->postcode }}</p>
+                <p>{{ $profile->address }}</p>
+                <p>{{ $profile->building ?? '' }}</p>
+            </div>
+        </div>
+        <div class="payment">
+            <form action="">
+                <table class="payment-form">
+                    <tr>
+                        <th>商品代金</th>
+                        <td><span class="yen">¥</span>{{ $item->price }}</td>
+                    </tr>
+                    <tr>
+                        <th>支払い方法</th>
+                        <td>コンビニ or カード</td>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
 @endsection
