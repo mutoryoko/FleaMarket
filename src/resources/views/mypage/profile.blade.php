23be230617dee17a5308d77b4b0923a043049994
muto ryoko <obng3prn3@gmail.com>
2025-08-06 17:13:39 +0900

    profile、editのレイアウト修正

--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -8,11 +8,13 @@
 
 @section('content')
 <div class="content">
-    <div class="profile">
-        <div class="profile-image__wrapper">
-            <img class="profile-image" src="{{ asset('storage/'.$profile->user_image) }}" alt="プロフィール画像">
+    <div class="profile__wrapper">
+        <div class="user-info">
+            <div class="user-image__wrapper">
+                <img class="user-img" src="{{ asset('storage/'.$profile->user_image) }}" alt="プロフィール画像">
+            </div>
+            <h2 class="user-name">{{ $user->name }}</h2>
         </div>
-        <h2 class="user-name">{{ $user->name }}</h2>
         <div class="edit-btn">
             <a class="edit-btn--link" href="{{ route('profile.edit') }}">プロフィールを編集</a>
         </div>
